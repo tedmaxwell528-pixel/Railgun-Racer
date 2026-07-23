a271e32d187cd10cca4bce7225e0e8897e2cf80e
Winson0330 <winsonw0330@gmail.com>
2026-07-23 19:24:14 -0400

    Move enemy car AI and gun firing mechanics to Winson scene

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -59,7 +59,7 @@ public class CarMovement : MonoBehaviour
     /// <param name="minMag"></param>
     /// <param name="maxMag"></param>
     /// <returns>The clamped vector</returns>
-    Vector3 ClampMagnitude(Vector3 vector, float minMag, float maxMag){
+    public Vector3 ClampMagnitude(Vector3 vector, float minMag, float maxMag){
         Vector3 clampedVector = vector.normalized;
         if (vector.magnitude < minMag){
             clampedVector *= minMag;
--- Assets/Scripts/EnemyCarAI.cs
+++ Assets/Scripts/EnemyCarAI.cs
@@ -11,12 +11,13 @@ public class EnemyCarAI : MonoBehaviour
     private int targetIndex;
     private int breadcrumbsCount = PlayerBreadcrumbs.breadcrumbs.Count;
     private float topSpeed = 10;
+    private float topSpeed = 120;
+    private float accelerationAmount = 5;
+    private float rotationSpeed = 120;
+    private float moveSpeed = 120;
 
     void FixedUpdate()
     {
-        if (breadcrumbsCount <= breadcrumbDelay)
-            return;
-
         // Follow an older point in the player's path
         targetIndex = Mathf.Clamp(breadcrumbsCount - breadcrumbDelay, 0, breadcrumbsCount - 1);
 
@@ -28,15 +29,17 @@ public class EnemyCarAI : MonoBehaviour
         float angle = Vector2.SignedAngle(transform.up, direction);
 
         // Steering value from -1 to 1
-        float totalRotation = Mathf.Clamp(angle / 45f, -1f, 1f);
+        float totalRotation = Mathf.Clamp(angle/45, -1f, 1f) * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0,0,totalRotation);
 
         // Apply AI inputs to the car controller
-        Vector2 acceleration = transform.up;
+        Vector2 acceleration = carController.ClampMagnitude(transform.up * accelerationAmount, accelerationAmount/2, accelerationAmount);
         Vector2 velocity = acceleration * Time.deltaTime;
         Vector2 driftVelocity = Vector3.ClampMagnitude(velocity, topSpeed);
         Quaternion stiffRotation = Quaternion.AngleAxis(totalRotation, Vector3.forward);
         Vector2 stiffVelocity = stiffRotation * driftVelocity; 
-        velocity = Vector2.Lerp(driftVelocity, stiffVelocity, 1);
+        velocity = Vector2.Lerp(driftVelocity, stiffVelocity, 1) * moveSpeed;
+        Debug.Log(velocity);
         
         transform.position += (Vector3)velocity * Time.deltaTime;
     }
